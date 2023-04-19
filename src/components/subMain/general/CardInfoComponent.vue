f02c2a802911338c52685a46c3cc1e1eb8d6bd03
totarochristian <totaro.christian.18@gmail.com>
2023-04-19 17:49:03 +0200

    Add empty card info component

---
+++ src/components/subMain/general/CardInfoComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="cardInfoComponent" class="col-11 col-md-8 col-lg-6 d-flex flex-column justify-content-center align-items-center">
+    Card info component
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CardInfoComponent'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
