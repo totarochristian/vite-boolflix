074d285af3362e5a884722f0e06c06fb5abf962b
totarochristian <totaro.christian.18@gmail.com>
2023-04-19 13:30:32 +0200

    Update store

--- src/data/store.js
+++ src/data/store.js
@@ -1,6 +1,12 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-  apiKey: 'd93c8fdeed5c57d0324a3c09acc8dd75',
-  baseUrl: 'https://api.themoviedb.org/3/',
+  apiSettings: {
+    apiKey: 'd93c8fdeed5c57d0324a3c09acc8dd75',
+    baseUrl: 'https://api.themoviedb.org/3/'
+  },
+  menuSettings: {
+    isSearchBarOpened: false,
+    sections: ["Home", "Film", "Serie Tv", "In arrivo", "Popolari", "Più votati"]
+  }
 });
