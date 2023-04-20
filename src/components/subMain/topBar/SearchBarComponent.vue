cea83f611dcd5e90740440519c624606dee057e7
totarochristian <totaro.christian.18@gmail.com>
2023-04-20 18:14:56 +0200

    Fix problems with the veil when the search page was open

--- src/components/subMain/topBar/SearchBarComponent.vue
+++ src/components/subMain/topBar/SearchBarComponent.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="searchBarClosingDiv" @click.self="store.menuSettings.isSearchBarOpened = false;" :class="{'removeVeil': store.settings.currentMenuSectionIndexOpened == -1}">
-    <div id="searchComponent" class="p-4 d-flex justify-content-center align-items-center">
+  <div id="searchBarClosingDiv" @click.self="store.menuSettings.isSearchBarOpened = false;" :class="{'d-none': store.settings.currentMenuSectionIndexOpened == -1}">
+  </div>
+  <div id="searchComponent" class="p-4 d-flex justify-content-center align-items-center">
       <div id="searchComponentBack" class="ps-4 d-flex align-items-center gap-4">
         <i class="fs-4 fa-solid fa-arrow-left" @click="store.menuSettings.isSearchBarOpened = false; store.settings.currentMenuSectionIndexOpened = 0;"></i>
       </div>
@@ -9,7 +10,6 @@
         <input class="col-12" type="text" name="searchInputText" id="searchInputText" :placeholder="store.settings.languages[store.settings.currentLanguageIndex].search[0]" v-model="store.menuSettings.textToSearch" @keyup="store.settings.currentMenuSectionIndexOpened = -1;">
       </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -34,32 +34,30 @@
     background-color: rgba(0, 0, 0, 0.7);
     z-index: 2000;
     overflow: hidden;
-    &.removeVeil{
-      background-color: rgba(0, 0, 0, 0);
+  }
+  #searchComponent{
+    position: relative;
+    top: 0;
+    left: 0;
+    z-index: 3000;
+    i{
+      cursor: pointer;
     }
-    #searchComponent{
-      position: relative;
-      top: 0;
-      left: 0;
-      i{
-        cursor: pointer;
-      }
-      #searchComponentActions{
-        input{
-          background-color: black;
-          border: 0;
-          color: #999999;
-          outline: 2px solid #999999;
-          &:hover{
-            color: white;
-            outline: 2px solid white;
-          }
+    #searchComponentActions{
+      input{
+        background-color: black;
+        border: 0;
+        color: #999999;
+        outline: 2px solid #999999;
+        &:hover{
+          color: white;
+          outline: 2px solid white;
         }
       }
-      #searchComponentBack{
-        position: absolute;
-        left: 0;
-      }
+    }
+    #searchComponentBack{
+      position: absolute;
+      left: 0;
     }
   }
 </style>
