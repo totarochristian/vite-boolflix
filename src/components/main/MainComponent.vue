639c5436e1b92dbceb6d16b983daad00108a6987
totarochristian <totaro.christian.18@gmail.com>
2023-04-20 10:17:04 +0200

    Update title of the carousels in the main component

--- src/components/main/MainComponent.vue
+++ src/components/main/MainComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <main class="px-4 d-flex flex-column justify-content-start align-items-center gap-4">
-    <CarouselComponent :endPoint="store.apiSettings.endPoints.film.popular" title="Popular film"/>
-    <CarouselComponent :endPoint="store.apiSettings.endPoints.film.topRated" title="Top rated film"/>
-    <CarouselComponent :endPoint="store.apiSettings.endPoints.film.nowPlaying" title="Now playing film"/>
-    <CarouselComponent :endPoint="store.apiSettings.endPoints.film.upcoming" title="Upcoming film"/>
-    <CarouselComponent :endPoint="store.apiSettings.endPoints.tv.popular" title="Popular TV series"/>
-    <CarouselComponent :endPoint="store.apiSettings.endPoints.tv.topRated" title="Top rated TV series"/>
-    <CarouselComponent :endPoint="store.apiSettings.endPoints.tv.airingToday" title="Airing today TV series"/>
+    <CarouselComponent :endPoint="store.apiSettings.endPoints.film.popular" title="Popular movies"/>
+    <CarouselComponent :endPoint="store.apiSettings.endPoints.film.topRated" title="Top rated movies"/>
+    <CarouselComponent :endPoint="store.apiSettings.endPoints.film.nowPlaying" title="Now playing movies"/>
+    <CarouselComponent :endPoint="store.apiSettings.endPoints.film.upcoming" title="Upcoming movies"/>
+    <CarouselComponent :endPoint="store.apiSettings.endPoints.tv.popular" title="Popular TV shows"/>
+    <CarouselComponent :endPoint="store.apiSettings.endPoints.tv.topRated" title="Top rated TV shows"/>
+    <CarouselComponent :endPoint="store.apiSettings.endPoints.tv.airingToday" title="TV shows airing today"/>
 
     <CardInfoComponent />
   </main>
