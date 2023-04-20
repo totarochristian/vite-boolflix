8a71d4a08520cc57d92548068bd60dda8ce5b90b
totarochristian <totaro.christian.18@gmail.com>
2023-04-20 18:11:31 +0200

    Add veil when user open the search bar but not wrote a term (+ user can click on the veil to close the search bar)

--- src/components/subMain/topBar/SearchBarComponent.vue
+++ src/components/subMain/topBar/SearchBarComponent.vue
@@ -1,15 +1,14 @@
 <template>
-  <div id="searchComponent" class="p-4 d-flex justify-content-center align-items-center">
-    <div id="searchComponentBack" class="ps-4 d-flex align-items-center gap-4">
-      <i class="fs-4 fa-solid fa-arrow-left" @click="store.menuSettings.isSearchBarOpened = false; store.settings.currentMenuSectionIndexOpened = 0;"></i>
+  <div id="searchBarClosingDiv" @click.self="store.menuSettings.isSearchBarOpened = false;" :class="{'removeVeil': store.settings.currentMenuSectionIndexOpened == -1}">
+    <div id="searchComponent" class="p-4 d-flex justify-content-center align-items-center">
+      <div id="searchComponentBack" class="ps-4 d-flex align-items-center gap-4">
+        <i class="fs-4 fa-solid fa-arrow-left" @click="store.menuSettings.isSearchBarOpened = false; store.settings.currentMenuSectionIndexOpened = 0;"></i>
+      </div>
+      <div id="searchComponentActions" class="d-flex align-items-center gap-1">
+        <i class="fs-4 fa-solid fa-magnifying-glass"></i>
+        <input class="col-12" type="text" name="searchInputText" id="searchInputText" :placeholder="store.settings.languages[store.settings.currentLanguageIndex].search[0]" v-model="store.menuSettings.textToSearch" @keyup="store.settings.currentMenuSectionIndexOpened = -1;">
+      </div>
     </div>
-    <div id="searchComponentActions" class="d-flex align-items-center gap-1">
-      <i class="fs-4 fa-solid fa-magnifying-glass"></i>
-      <input class="col-12" type="text" name="searchInputText" id="searchInputText" :placeholder="store.settings.languages[store.settings.currentLanguageIndex].search[0]" v-model="store.menuSettings.textToSearch" @keyup="store.settings.currentMenuSectionIndexOpened = -1;">
-    </div>
-  </div>
-  <div>
-    
   </div>
 </template>
 
@@ -26,28 +25,41 @@
 </script>
 
 <style lang="scss" scoped>
-  #searchComponent{
+  #searchBarClosingDiv{
     width: 100%;
-    height: 100%;
-    position: relative;
-    i{
-      cursor: pointer;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.7);
+    z-index: 2000;
+    overflow: hidden;
+    &.removeVeil{
+      background-color: rgba(0, 0, 0, 0);
     }
-    #searchComponentActions{
-      input{
-        background-color: black;
-        border: 0;
-        color: #999999;
-        outline: 2px solid #999999;
-        &:hover{
-          color: white;
-          outline: 2px solid white;
+    #searchComponent{
+      position: relative;
+      top: 0;
+      left: 0;
+      i{
+        cursor: pointer;
+      }
+      #searchComponentActions{
+        input{
+          background-color: black;
+          border: 0;
+          color: #999999;
+          outline: 2px solid #999999;
+          &:hover{
+            color: white;
+            outline: 2px solid white;
+          }
         }
       }
-    }
-    #searchComponentBack{
-      position: absolute;
-      left: 0;
+      #searchComponentBack{
+        position: absolute;
+        left: 0;
+      }
     }
   }
 </style>
