0d80432cbf50de3d5268dd13acebf46fbcedf629
totarochristian <totaro.christian.18@gmail.com>
2023-04-20 16:15:54 +0200

    Update film component, transform the matrix component to carousel component

--- src/components/subMain/pages/FilmComponent.vue
+++ src/components/subMain/pages/FilmComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-100 h-100" :class="{'d-none': !store.settings.currentMenuSectionIndexOpened == 1}">
-    <MatrixComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.film.popular" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[0]"/>
+    <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.film.popular" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[0]"/>
     <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.film.topRated" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[1]"/>
     <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.film.nowPlaying" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[2]"/>
     <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.film.upcoming" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[3]"/>
@@ -9,13 +9,11 @@
 
 <script>
   import CarouselComponent from '../main/CarouselComponent.vue';
-  import MatrixComponent from '../main/MatrixComponent.vue';
   import { store } from '../../../data/store';
   export default {
     name: 'FilmComponent',
     components:{
-      CarouselComponent,
-      MatrixComponent
+      CarouselComponent
     },
     data(){
       return{
