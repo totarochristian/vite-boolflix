7e9e87bc6e1120ab648680ed0288d9f96a8c6500
totarochristian <totaro.christian.18@gmail.com>
2023-04-20 16:35:42 +0200

    Update stars component to display empty stars

--- src/components/subMain/general/StarsComponent.vue
+++ src/components/subMain/general/StarsComponent.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="d-flex gap-2">
-    <i class="fa-solid fa-star" v-for="num in Math.trunc(voteAverage)"></i>
-    <i class="fa-solid fa-star-half-stroke" v-if="voteAverage % 1 >= 0.5"></i>
+    <i class="fa-solid fa-star" v-for="num in filledStar"></i>
+    <i class="fa-solid fa-star-half-stroke" v-if="halfStar"></i>
+    <i class="fa-regular fa-star" v-for="num in emptyStar"></i>
   </div>
 </template>
 
@@ -10,6 +11,18 @@
     name: 'StarsComponent',
     props:{
       voteAverage: Number
+    },
+    data(){
+      return{
+        filledStar: 0,
+        halfStar: 0,
+        emptyStar: 0
+      }
+    },
+    mounted(){
+      this.filledStar = Math.trunc(this.voteAverage);
+      this.halfStar = this.voteAverage % 1 >= 0.5;
+      this.emptyStar = 10 - this.filledStar - this.halfStar;
     }
   }
 </script>
