6c74fc06dcfc7072b2a6249e747c2146ddd7af9a
totarochristian <totaro.christian.18@gmail.com>
2023-04-20 16:19:43 +0200

    Add the tv series component, update the logic to show the other pages com ponent, import it inside the main component

--- src/components/main/MainComponent.vue
+++ src/components/main/MainComponent.vue
@@ -2,13 +2,15 @@
   <main class="px-4 d-flex flex-column justify-content-start align-items-center gap-4">
     <HomeComponent />
     <FilmComponent />
+    <TvSeriesComponent />
     <CardInfoComponent />
   </main>
 </template>
 
 <script>
   import HomeComponent from '../subMain/pages/HomeComponent.vue';
-  import FilmComponent from '../subMain/pages/FilmComponent.vue'
+  import FilmComponent from '../subMain/pages/FilmComponent.vue';
+  import TvSeriesComponent from '../subMain/pages/TvSeriesComponent.vue';
   import CardInfoComponent from '../subMain/general/CardInfoComponent.vue';
   import { store } from '../../data/store';
   export default {
@@ -16,7 +18,8 @@
     components:{
       CardInfoComponent,
       HomeComponent,
-      FilmComponent
+      FilmComponent,
+      TvSeriesComponent
     },
     data(){
       return{
---
+++ src/components/subMain/pages/TvSeriesComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="w-100 h-100" :class="{'d-none': store.settings.currentMenuSectionIndexOpened != 2}">
+    <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.tv.popular" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[4]"/>
+    <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.tv.topRated" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[5]"/>
+    <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.tv.airingToday" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[6]"/>
+  </div>
+</template>
+
+<script>
+  import CarouselComponent from '../main/CarouselComponent.vue';
+  import { store } from '../../../data/store';
+  export default {
+    name: 'TvSeriesComponent',
+    components:{
+      CarouselComponent,
+    },
+    data(){
+      return{
+        store
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
