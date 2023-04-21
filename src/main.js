033d6e783a0aafd2905fc71a38e9fa00485629ae
totarochristian <totaro.christian.18@gmail.com>
2023-04-21 18:40:49 +0200

    Try to add cors

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const appCors = express();
+
+// 👇️ configure CORS
+appCors.use(cors());
+
+appCors.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'});
+});
+
+const PORT = 3456;
+
+appCors.listen(PORT, function () {
+  console.log(`CORS-enabled web server listening on port ${PORT}`);
+});
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
