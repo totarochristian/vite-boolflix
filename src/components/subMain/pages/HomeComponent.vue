37f79a4f36a190c865451953b52cfa6408c2d45b
totarochristian <totaro.christian.18@gmail.com>
2023-04-22 11:40:16 +0200

    Fix v-if logic in home component

--- src/components/subMain/pages/HomeComponent.vue
+++ src/components/subMain/pages/HomeComponent.vue
@@ -6,7 +6,7 @@
     <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.film.nowPlaying" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[2]" v-if="pageIndex == 0 || pageIndex == 1"/>
     <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.film.upcoming" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[3]" v-if="pageIndex == 0 || pageIndex == 1 || pageIndex == 3"/>
     <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.tv.popular" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[4]" v-if="pageIndex == 0 || pageIndex == 2 || pageIndex == 4"/>
-    <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.tv.topRated" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[5]" v-if="pageIndex == 0 || pageIndex == 2 pageIndex == 5 "/>
+    <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.tv.topRated" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[5]" v-if="pageIndex == 0 || pageIndex == 2 || pageIndex == 5 "/>
     <CarouselComponent :languageIndex="store.settings.currentLanguageIndex" :endPoint="store.apiSettings.endPoints.tv.airingToday" :title="store.settings.languages[store.settings.currentLanguageIndex].carouselsTitles[6]" v-if="pageIndex == 0 || pageIndex == 2 || pageIndex == 3"/>
   </div>
 </template>
